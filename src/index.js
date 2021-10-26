a291e02414a780ecbfc319a013c0f2129924c637
unknown <okpara_tochukwu@yahoo.com>
2021-10-26 07:30:07 +0100

    setup webpack and linters for to do list

--- src/index.js
+++ src/index.js
@@ -2,18 +2,18 @@ import _ from 'lodash';
 import './style.css';
 import Wallpaper from './children1.jpg';
 
- function component() {
-   const element = document.createElement('div');
+function component() {
+  const element = document.createElement('div');
   // Lodash, now imported by this script
-   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-   element.classList.add('hello');
-     // Add the image to our existing div.
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+  // Add the image to our existing div.
   const myIcon = new Image();
   myIcon.src = Wallpaper;
 
   element.appendChild(myIcon);
 
-   return element;
- }
+  return element;
+}
 
- document.body.appendChild(component());
+document.body.appendChild(component());
